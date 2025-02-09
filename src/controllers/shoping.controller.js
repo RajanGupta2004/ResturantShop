0053cc4c9d617840a5243d6355907364db409663
Rajan Gupta <rajan.gupta@cirrius.com>
2025-02-09 10:06:09 +0530

    shoping realted api completed

--- src/controllers/shoping.controller.js
+++ src/controllers/shoping.controller.js
@@ -9,7 +9,7 @@ export const GetFoodAvailibility = async (req, res) => {
         const pincode = req.params.pincode;
 
         const result = await Vandor.find({ pincode: pincode, serviceAvailable: true })
-        .populate("foods").sort([['rating' , "descending"]])
+            .populate("foods").sort([['rating', "descending"]])
 
         if (result.length < 0) {
             return res.status(404).json(new ApiResponse(404, "Food not found"))
@@ -29,12 +29,12 @@ export const GetFoodAvailibility = async (req, res) => {
 
 export const GetTopResturant = async (req, res) => {
     try {
-        
+
         const pincode = req.params.pincode;
         console.log(pincode)
 
         const result = await Vandor.find({ pincode: pincode, serviceAvailable: true })
-       
+
 
         if (result.length <= 0) {
             return res.status(404).json(new ApiResponse(404, "Resturant  not found"))
@@ -51,6 +51,28 @@ export const GetTopResturant = async (req, res) => {
 export const GetFoodIn30Min = async (req, res) => {
     try {
 
+        const pincode = req.params.pincode;
+        if (!pincode) {
+            return res.status(404).json(new ApiResponse(400, "pincode required..."))
+        }
+
+        const result = await Vandor.find({ pincode: pincode, serviceAvailable: true }).populate("foods")
+
+        if (result.length <= 0) {
+            return res.status(404).json(new ApiResponse(404, "Resturant  not found"))
+        }
+
+        const AllfoodArr = []
+
+        const foodIn30min = result.map((vandor) => (
+            vandor.foods.filter((item) => item.readyTime <= 30)
+        ))
+
+        if(foodIn30min.length<=0){
+            return res.status(404).json(new ApiResponse(404, "Resturant  not found"))
+        }
+        return res.status(200).json(new ApiResponse(200, "Food  in 30min data fetch successfully", foodIn30min))
+
     } catch (error) {
         console.log("Error in GetFoodIn30Min ", error)
         return res.status(500).json(new ApiResponse(500, "Internal server error "))
@@ -60,14 +82,48 @@ export const GetFoodIn30Min = async (req, res) => {
 export const GetSearchFood = async (req, res) => {
     try {
 
+        const pincode = req.params.pincode;
+        if (!pincode) {
+            return res.status(404).json(new ApiResponse(400, "pincode required..."))
+        }
+
+        const result = await Vandor.find({ pincode: pincode, serviceAvailable: true }).populate("foods")
+
+
+        const searchFood = result.map((vandor)=>vandor.foods)
+
+        if(searchFood.length<0){
+            return res.status(404).json(new ApiResponse(404 , "Food not found"))
+        }
+
+        return res.status(200).json(new ApiResponse(200, "Food  in 30min data fetch successfully", searchFood))
+
+
+        if (result.length <= 0) {
+            return res.status(404).json(new ApiResponse(404, "Resturant  not found"))
+        }
+
     } catch (error) {
         console.log("Error in GetFoodAvailibility ", error)
         return res.status(500).json(new ApiResponse(500, "Internal server error "))
 
     }
 }
+
+
 export const ResturantById = async (req, res) => {
     try {
+
+        const id = req.params.id
+
+        const vandor = await Vandor.findById(id).populate("foods")
+
+        if(!vandor){
+            return res.status(404).json(new ApiResponse(404 , "Vandor not found..."))
+        }
+        return res.status(200).json(new ApiResponse(200, "Food  in 30min data fetch successfully", vandor))
+
+        
 
     } catch (error) {
         console.log("Error in ResturantById ", error)
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -10,9 +10,7 @@ router.post("/vandor" , createVendor )
 router.get("/vandors" ,getVendors )
 router.get("/vandor/:id" ,getVendorById )
 
-router.get("/" ,(req , res)=>{
-    return res.status(200).json(new ApiResponse(200 , "success"))
-} )
+
 
 
 
